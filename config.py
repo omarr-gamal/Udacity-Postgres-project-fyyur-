6b5e35291257199afd279bf325c40222661e4704
Omar Gamal <omogamal2002@gmail.com>
2025-11-16 23:03:24 +0200

    generate db url from environment variables

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 SECRET_KEY = os.urandom(32)
+
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
-
-# Connect to the database
-
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:gsaiho2017@localhost:5432/fyyur'
+db_base_url=os.environ.get("DB_BASE_UR", "localhost:5432")
+db_username=os.environ.get("DB_USERNAME", "postgres")
+db_password=os.environ.get("DB_PASSWORD", "")
+db_name=os.environ.get("DB_NAME", "fyyur")
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_username}:{db_password}@{db_base_url}/{db_name}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
